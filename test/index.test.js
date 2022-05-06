5ab394bf4b4fcfc57dec3fdcdedcca35e69109d4
Brian Spencer <brsj13@gmail.com>
2022-05-06 08:27:57 -0700

    add testing for when letters contains incorrect data & letters is missing

--- test/index.test.js
+++ test/index.test.js
@@ -17,6 +17,33 @@ describe('GET /', () => {
     })
 
     describe('when letters is missing', () => {
-        //should respond with status code of 400
+
+        test('should respond with a 400 status code', async () => {
+            const response = await request(app).get("/").send()
+            expect(response.statusCode).toBe(400)
+        })
+    })
+
+    describe('given letters containing incorrect data', () => {
+        
+        test('if letters is not a string should respond with 400 status code', async () => {
+            const response = await request(app).get("/").send({letters: ['a', 'b', 'c']})
+            expect(response.statusCode).toBe(400)
+        })
+
+        test('if letters is less than 1 char length respond with 400 status code', async () => {
+            const response = await request(app).get("/").send({letters: ''})
+            expect(response.statusCode).toBe(400)
+        })
+
+        test('if letters is more than 5 char length respond with 400 status code', async () => {
+            const response = await request(app).get("/").send({letters: 'abcdef'})
+            expect(response.statusCode).toBe(400)
+        })
+
+        test('if letters contains anything not (a-z, A-Z, ?) respond with 400 status code', async () => {
+            const response = await request(app).get("/").send({letters: '@*%12'})
+            expect(response.statusCode).toBe(400)
+        })
     })
 })
