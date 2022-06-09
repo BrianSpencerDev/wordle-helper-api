412d413a7c62d9bba8917f0bf71445e4497dafc1
Brian Spencer <brsj13@gmail.com>
2022-06-08 18:38:27 -0700

    add https

--- src/server.js
+++ src/server.js
@@ -1,5 +1,40 @@
 const app = require('./index');
+import {readFile} from 'fs/promises';
+import http from "http";
+import https from "https";
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000')
+// })
+
+const httpServer = http.createServer(app);
+  
+httpServer.listen(3000, () => {
+    logHandler.log("http", "http server listening on port 3000");
+});
+
+if (process.env.NODE_ENV === "production") {
+    const privateKey = await readFile(
+    "/etc/letsencrypt/live/wordleapi.brian-spencer.com/privkey.pem",
+    "utf8"
+    );
+    const certificate = await readFile(
+    "/etc/letsencrypt/live/wordleapi.brian-spencer.com/cert.pem",
+    "utf8"
+    );
+    const ca = await readFile(
+    "/etc/letsencrypt/live/wordleapi.brian-spencer/chain.pem",
+    "utf8"
+    );
+
+    const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca,
+    };
+
+    const httpsServer = https.createServer(credentials, app);
+
+    httpsServer.listen(3080, () => {
+    logHandler.log("http", "https server listening on port 3080");
+    })}
