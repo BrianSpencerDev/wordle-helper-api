72c15395a842920a101bd343a83cfef38ac4d4d1
Brian Spencer <brsj13@gmail.com>
2022-06-08 18:50:00 -0700

    fix await/async in server.js

--- src/server.js
+++ src/server.js
@@ -7,34 +7,36 @@ const https = require("https");
 //     console.log('Server is running on port 3000')
 // })
 
-const httpServer = http.createServer(app);
+async () => {
+    const httpServer = http.createServer(app);
   
-httpServer.listen(3000, () => {
-    logHandler.log("http", "http server listening on port 3000");
-});
+    httpServer.listen(3000, () => {
+        logHandler.log("http", "http server listening on port 3000");
+    });
 
-if (process.env.NODE_ENV === "production") {
-    const privateKey = await readFile(
-    "/etc/letsencrypt/live/wordleapi.brian-spencer.com/privkey.pem",
-    "utf8"
-    );
-    const certificate = await readFile(
-    "/etc/letsencrypt/live/wordleapi.brian-spencer.com/cert.pem",
-    "utf8"
-    );
-    const ca = await readFile(
-    "/etc/letsencrypt/live/wordleapi.brian-spencer/chain.pem",
-    "utf8"
-    );
+    if (process.env.NODE_ENV === "production") {
+        const privateKey = await readFile(
+        "/etc/letsencrypt/live/wordleapi.brian-spencer.com/privkey.pem",
+        "utf8"
+        );
+        const certificate = await readFile(
+        "/etc/letsencrypt/live/wordleapi.brian-spencer.com/cert.pem",
+        "utf8"
+        );
+        const ca = await readFile(
+        "/etc/letsencrypt/live/wordleapi.brian-spencer/chain.pem",
+        "utf8"
+        );
 
-    const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca,
-    };
+        const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca,
+        };
 
-    const httpsServer = https.createServer(credentials, app);
+        const httpsServer = https.createServer(credentials, app);
 
-    httpsServer.listen(3080, () => {
-    logHandler.log("http", "https server listening on port 3080");
-    })}
+        httpsServer.listen(3080, () => {
+        logHandler.log("http", "https server listening on port 3080");
+        })}
+}
